cc29c4d460eef2e9de8a4957b08e5f6b2818984a
Jograj singh <89298550+Jukes312@users.noreply.github.com>
2022-12-26 12:32:07 +0000

    Bug fix local memory

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -5,34 +5,11 @@ import { createContext, useState, useEffect } from 'react';
 const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
-    const [posts, setPosts] = useState(JSON.parse(localStorage.getItem('posts')))
+    const [posts, setPosts] = useState(JSON.parse(localStorage.getItem('posts'))||[])
     const [search, setSearch] = useState('');
     const [searchResults, setSearchResults] = useState([]);
 
     useEffect(() => {
-        if (JSON.parse(localStorage.getItem('posts')) === null) {
-           
-        
-            localStorage.setItem('posts', JSON.stringify([{
-                id: 1,
-                title: "1st post",
-                datetime: "July 16, 2021 11:47:39 AM",
-                body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            },
-            {
-                id: 2,
-                title: "Second post",
-                datetime: "July 16, 2021 11:47:48 AM",
-                body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. two"
-            },
-            ]))
-        }
-        
-    },[])
-
-
-    useEffect(() => {
-        
         const filteredResults = posts.filter((post) =>
             ((post.body).toLowerCase()).includes(search.toLowerCase())
             || ((post.title).toLowerCase()).includes(search.toLowerCase()));
